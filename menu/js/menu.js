60ce411ebec64b0322a6e233c29e0daa38db5437
CAXAPOKvanya <bogratov02@gmail.com>
2020-12-27 19:07:06 +0900

    add shadow for settings

--- menu/js/menu.js
+++ menu/js/menu.js
@@ -1,7 +1,7 @@
 var menu = new Vue({
     el: '#body',
     data: {
-        page: 0, // 0 - главная, 1 - настройки
+        page: 1, // 0 - главная, 1 - настройки
         enableVR: false, //true, чтобы вр подрубить
     },
 }); 
