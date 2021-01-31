a430e25a5de09f6cb4310af9a4c3beff42947850
IDarkLegendI <igor-boyar2011@yandex.ru>
2021-02-01 01:18:43 +0300

    Мультиязык для выбора машин

--- menu/js/menu.js
+++ menu/js/menu.js
@@ -101,6 +101,7 @@ var menu = new Vue({
             buy: 'BUY',
             select: 'SELECT',
             selected: 'SELECTED',
+            none: 'NOT SELECTED',
         },
         avatars: {
             "A": "0",
@@ -384,6 +385,7 @@ var menu = new Vue({
                 buy: 'КУПИТЬ',
                 select: 'ВЫБРАТЬ',
                 selected: 'ВЫБРАНО',
+                none: 'НЕ ВЫБРАНО',
             }
         }, 
         setCar(model)
@@ -508,7 +510,7 @@ else
         menu.requestsOut = ['DarkLegend']
         menu.fUpdateLobby([{name: "Player", ava: 3, ready: 01}, {name: "Resce", ava: 2, ready: 0}, {name: "DarkLegend", ava: 1, ready: 1}])
         // menu.fUpdateLobby([{name: "Player-1", ava: 1}, {name: "Player-2", ava: 2}, {name: "DarkLegend", ava: 1}]) // Если хочешь пригласить чтобы кнопка появилась
-        menu.switchPage(0, 3)  
+        menu.switchPage(2, 0)  
     }, 100)
     document.getElementById('body').style.backgroundImage = "url(./img/fon.png)" 
     document.body.style.cursor = "default" 
