36e0893c1582d238a885e25a9dacfb3479f943ca
CAXAPOKvanya <bogratov02@gmail.com>
2021-02-14 22:11:20 +0900

    Кнопка переключения языков в авторизации + меню (настройки)

--- menu/js/menu.js
+++ menu/js/menu.js
@@ -618,7 +618,7 @@ else
         // menu.requestsOut = ['DarkLegend']
         menu.fUpdateLobby([{name: "Player", ready: 1}, {name: "Resce", ready: -1}, {name: "DarkLegend", ready: 1}])
         // menu.fUpdateLobby([{name: "Player-1", ava: 1}, {name: "Player-2", ava: 2}, {name: "DarkLegend", ava: 1}]) // Если хочешь пригласить чтобы кнопка появилась
-        menu.switchPage(0, 0)  
+        menu.switchPage(1, 0)  
         // menu.fInviteToLobby(1, [{name: "Player", ready: 0}, {name: "Resce", ready: 0}, {name: "DarkLegend", ready: 1}])
         // menu.statusGame = true;
     }, 100)
