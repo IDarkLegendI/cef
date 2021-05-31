d7b7963d624067642a3b9ed7a045018fa42407bf
DarkLegend <42875136+IDarkLegendI@users.noreply.github.com>
2021-05-31 18:33:43 +0300

    Menu: +метод для контрастного текста кнопки применения тюнинга

---
+++ menu/js/tools.js
@@ -0,0 +1,9 @@
+function contrastingColor(rgb) 
+{
+    // console.log(`luma: ${luma(rgb)};`) 
+    return (luma(rgb) >= 165) ? '000' : 'fff';
+}
+function luma(rgb) // color can be a hx string or an array of RGB values 0-255
+{
+    return (0.2126 * rgb[0]) + (0.7152 * rgb[1]) + (0.0722 * rgb[2]); // SMPTE C, Rec. 709 weightings
+}
