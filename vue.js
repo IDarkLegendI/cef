6570745ce9355a72341da788f2fdc3aec0a3e63a
DarkLegend <42875136+IDarkLegendI@users.noreply.github.com>
2021-06-12 02:38:55 +0300

    Vue: должна быть повышена стабильность срабатываний кликов

--- vue.js
+++ vue.js
@@ -7524,30 +7524,33 @@
     // the solution is simple: we save the timestamp when a handler is attached,
     // and the handler would only fire if the event passed to it was fired
     // AFTER it was attached.
-    if (useMicrotaskFix) {
-      var attachedTimestamp = currentFlushTimestamp;
-      var original = handler;
-      handler = original._wrapper = function (e) {
-        if (
-          // no bubbling, should always fire.
-          // this is just a safety net in case event.timeStamp is unreliable in
-          // certain weird environments...
-          e.target === e.currentTarget ||
-          // event is fired after handler attachment
-          e.timeStamp >= attachedTimestamp ||
-          // bail for environments that have buggy event.timeStamp implementations
-          // #9462 iOS 9 bug: event.timeStamp is 0 after history.pushState
-          // #9681 QtWebEngine event.timeStamp is negative value
-          e.timeStamp <= 0 ||
-          // #9448 bail if event is fired in another document in a multi-page
-          // electron/nw.js app, since event.timeStamp will be using a different
-          // starting reference
-          e.target.ownerDocument !== document
-        ) {
-          return original.apply(this, arguments)
-        }
-      };
-    }
+
+    //START COMMENT
+    // if (useMicrotaskFix) {
+    //   var attachedTimestamp = currentFlushTimestamp;
+    //   var original = handler;
+    //   handler = original._wrapper = function (e) {
+    //     if (
+    //       // no bubbling, should always fire.
+    //       // this is just a safety net in case event.timeStamp is unreliable in
+    //       // certain weird environments...
+    //       e.target === e.currentTarget ||
+    //       // event is fired after handler attachment
+    //       e.timeStamp >= attachedTimestamp ||
+    //       // bail for environments that have buggy event.timeStamp implementations
+    //       // #9462 iOS 9 bug: event.timeStamp is 0 after history.pushState
+    //       // #9681 QtWebEngine event.timeStamp is negative value
+    //       e.timeStamp <= 0 ||
+    //       // #9448 bail if event is fired in another document in a multi-page
+    //       // electron/nw.js app, since event.timeStamp will be using a different
+    //       // starting reference
+    //       e.target.ownerDocument !== document
+    //     ) {
+    //       return original.apply(this, arguments)
+    //     }
+    //   };
+    // }
+    //END COMMENT
     target$1.addEventListener(
       name,
       handler,
