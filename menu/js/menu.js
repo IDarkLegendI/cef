cec44ef3302a70f1ffea22a4547dc8ec6992036b
DarkLegend <42875136+IDarkLegendI@users.noreply.github.com>
2021-07-07 00:56:12 +0300

    Menu: сделано отображение cashBackа за продажу машины

--- menu/js/menu.js
+++ menu/js/menu.js
@@ -1715,7 +1715,7 @@ if ('alt' in window) {
         // menu.wsWin = true
         menu.news = [{name: "111111111111111112222222222222222222222222222222222222222222222222222222212224", type: true}, 
         {name: "Вам поступил запрос в друзья", type: false},{name: "Вам поступил запрос в друзь2", type: false},{name: "Вам поступил запрос в друзь3", type: false},{name: "Вам поступил запрос в друзь4", type: false},{name: "Вам поступил запрос в друзь5", type: false},{name: "Вам поступил запрос в друзь6", type: false},{name: "Вам поступил запрос в друзь7", type: false},{name: "Вам поступил запрос в друзь8", type: false},{name: "Вам поступил запрос в друзь9", type: false},]
-        menu.switchPage(2, 0)
+        menu.switchPage(2, 2)
         menu.plusMoney = 5
         menu.bonusMoney = 5
         menu.wsWin = true
@@ -1727,6 +1727,8 @@ if ('alt' in window) {
     menu.disableCursor = true
     menu.placeAll = 3;
     menu.place = 2;
+    menu.vip = 'deluxe'
+    menu.carsPointer = 67
     // menu.cars[1].price = 0
     // menu.anyVar = 34 //УБРАТЬ!
         setTimeout(() => { 
