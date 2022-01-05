195d0bfc3df0b3e124c736c0284eff013ab06aef
DarkLegend <42875136+IDarkLegendI@users.noreply.github.com>
2022-01-05 21:57:22 +0300

    Hud: подключена новая система загрузки

--- hud/js/hud.js
+++ hud/js/hud.js
@@ -466,7 +466,7 @@ var hud = new Vue({
             min = Math.ceil(min);
             max = Math.floor(max);
             return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-          }
+        },
     },
 })
 
